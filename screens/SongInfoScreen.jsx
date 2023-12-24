870a9e9f1400d236d2ce7707b6df78ebffb8b345
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-24 18:08:55 +0500

    'initial

---
+++ screens/SongInfoScreen.jsx
@@ -0,0 +1,15 @@
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SongInfoScreen() {
+  return (
+    <View>
+      <Text>SongInfoScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
